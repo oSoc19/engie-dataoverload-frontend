1c0a47053abe3ea802917691223caf94b4a29cb4
jodiDL <jodi@jodideloof.be>
2019-07-03 15:02:49 +0200

    basic network calls

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-  render() {
-    return (
-        <div className="container content">
-          <h2>Home</h2>
-          <hr/>
-        </div>
-    );
-  }
+    constructor() {
+        super();
+        //this.state = {testdata: []};
+    }
+
+    componentDidMount() {
+        /*fetch('http://localhost:9000/api/test').then(
+            results => {
+                return results.json();
+            }
+        ).then(
+            data => {
+                let list = data.map((x) => {
+                    return (
+                        <div>
+                            <p>{x.customer_id}, {x.value}, {x.timestamp_begin}</p>
+                        </div>
+                    );
+                });
+
+                this.setState({ testdata: list });
+            }
+        )*/
+    }
+
+    render() {
+        return (
+            <div className="container content">
+                <h2>Home</h2>
+                <hr />
+                <div>
+                    {/*this.state.testdata*/}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;
