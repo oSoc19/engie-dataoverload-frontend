635fda6c41d833319df108233f20245c658e3d3b
jodiDL <jodi@jodideloof.be>
2019-07-04 11:00:43 +0200

    connect info to api

--- client/src/components/info.js
+++ client/src/components/info.js
@@ -5,31 +5,36 @@ class Info extends Component {
     constructor() {
         super();
         this.state = {
-            funFacts: 
-                [
-                    ["fas fa-fire ", "fact1", "676"], 
-                    ["fas fa-plug", "fact2", "6336"], 
-                    ["fas fa-tint", "fact3", "476"]
-                ]
+            funFacts: []
         }
     }
 
     componentDidMount() {
-        let list = this.state.funFacts.map((fact) => {
-            return (
-                <div key={fact[1]} className="col-md-4">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <i className={fact[0] + " fa-5x"}></i>
-                            <h5 className="card-title">{fact[1]}</h5>
-                            <p className="card-text">{fact[2]}</p>
+        fetch('http://localhost:9000/api/allaverages').then(
+            results => {
+                return results.json();
+            }
+        ).then(
+            data => {
+                console.log(data);
+                let list = data.map((fact) => {
+                    console.log(fact);
+                    
+                    return (
+                        <div key={fact.name} className="col-md-4">
+                            <div className="card">
+                                <div className="card-body text-center">
+                                    <i className={fact.icon + " fa-5x"}></i>
+                                    <h5 className="card-title">{fact.name}</h5>
+                                    <p className="card-text">{fact.value}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            );
-        });
+                    );
+                });
 
-        this.setState({ funFacts: list });
+                this.setState({ funFacts: list }); 
+            });
     }
 
     render() {
