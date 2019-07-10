062c5bc9c0dfddf7ab1f0c6571cf24eab4712912
Bao Truong <duybao.truong@student.vinci.be>
2019-07-10 16:58:35 +0200

    add comparator functions

--- client/src/controller/comparatorController.js
+++ client/src/controller/comparatorController.js
@@ -7,6 +7,53 @@ class ComparatorController{
     getResults(){
       return 42;
     };
+
+    getConsEstimation(values){
+
+      //Reading values from the quiz
+        //request.get("family_size");
+        //get 
+    
+      //Arrays declaration
+      let results = [];
+      let waterData = getWaterConsEstimation(size);
+      let gasData = getGasConsEstimation(size);
+      let elecData = getElecConsEstimation(size);
+    
+      //Concatenate all arrays into one
+      results.concat(waterData);
+      results.concat(gasData);
+      results.concat(elecData);
+    
+      response.status(200).send(results);
+    };
+    
+    getWaterConsEstimatio(size){
+    
+      //Variables declaration
+      let results = [];
+      let averageWebCons = 40; //in m³, per year, per person
+      
+    
+    
+      results.push({hello : "test"});
+    
+      return results;
+    };
+    
+    getGasConsEstimation(size){
+      let results = [];
+      results.push({hello : "test"});
+    
+      return results;
+    };
+    
+    getElecConsEstimation(size){
+      let results = [];
+      results.push({hello : "test"});
+    
+      return results;
+    };
 }
 
 export default ComparatorController;
