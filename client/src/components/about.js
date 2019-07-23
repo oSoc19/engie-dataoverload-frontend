efd6222bd9a6148e9815a8b043412563b4b316b5
John de Wasseige <johndewasseige@me.com>
2019-07-23 10:00:21 +0200

    summary in about page and compress pictures

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -11,7 +11,7 @@ class About extends Component {
             {
                 name: 'Bao',
                 img: './people/bao.jpg',
-                github: 'baotrong',
+                github: 'dbtruong',
                 linkedin: 'bao-truong-8752a015b'
             },
             {
@@ -93,9 +93,13 @@ class About extends Component {
                             This project took place at the Belgian edition 
                             of <a className="about_ref" href="https://2019.summerofcode.be/">Open Summer of Code 2019</a>,
                             with the collaboration of <a className="about_ref" href="https://www.engie-electrabel.be/fr/">Engie</a>.
-                            Our goal is to compare electricity consumption 
-                            of people with and without solar panels via a line chart. This website also
-                            displays aggregated information about energy and water consumption.
+                            Our goal is to deliver insights based on consumption data from consumers. 
+                            The data includes variables such as their electric consumption and solar panel production.
+                            For this purpose we provided some <a className="about_ref" href="/info">key numbers</a> on behaviours of
+                            consumers in Belgium. Visualizations comparing the <a className="about_ref" href="/solar">electricity 
+                            consumption of houses with and without solar panels</a> have also been implemented for different time frames.
+                            Lastly, a <a className="about_ref" href="/quiz">quiz</a> is proposed to allow users to evaluate their consumption
+                            and compare it with the average belgian consumption.
                         </p>
                     </div>
                 </div>
@@ -106,7 +110,7 @@ class About extends Component {
                     {this.state.team_members}                
                 </div>
 
-                <h4 className="text-center">And a fancy bar chart group picture</h4>
+                <h4 className="text-center">Our fancy bar chart group picture</h4>
                 <hr />
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
